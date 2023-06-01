3f58afaf981a77d8dd274d48f82b6a2831424677
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2023-06-01 11:52:34 -0400

    add constructor and array

---
+++ script.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+/**
+ * 
+ * @param {String} title 
+ * @param {String} author 
+ * @param {Number} pages 
+ * @param {boolean} read 
+ */
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        if(read){
+            return `${this.title} by ${this.author}, ${this.pages} pages, read`
+        }
+        else{
+            return `${this.title} by ${this.author}, ${this.pages} pages, not yet read`
+        }
+    }
+}
+
