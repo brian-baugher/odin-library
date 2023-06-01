590ff99dede7efb206a97752724b1073af8b108c
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2023-06-01 12:17:37 -0400

    add display books function

--- script.js
+++ script.js
@@ -28,3 +28,28 @@ function Book(title, author, pages, read){
 function addBookToLibrary(book){
     myLibrary.push(book)
 }
+
+function displayBooks(){
+    for(let book of myLibrary){
+        const card = document.createElement('div')
+        card.classList.add('book')
+
+        const inf = document.createElement('p')
+        inf.textContent = book.info()
+        card.classList.add('info')
+        card.appendChild(inf)
+
+        const container = document.getElementById('books')
+        container.appendChild(card)
+    }
+}
+
+let b1 = new Book('book 1', 'brian', 50, false)
+let b2 = new Book('book 2', 'betsy', 100, true)
+let b3 = new Book('book 3', 'sean', 150, true)
+
+addBookToLibrary(b1)
+addBookToLibrary(b2)
+addBookToLibrary(b3)
+
+displayBooks()
